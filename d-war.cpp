742768e524805ea2820c4c24e6cfb55623f52ab2
cristian.vijdea <cristian.vijdea@ligaac.ro>
2017-05-16 03:01:26 +0300

    Solved problem D - War.

--- d-war.cpp
+++ d-war.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
-#include <string>
+#include <memory>
 #include <fstream>
 #include <vector>
 #include <algorithm>
 #include <queue>
 #include <stack>
+#include <set>
+#include <map>
 
+struct FriendGroup
+{
+    std::set<int> friends;
+    std::weak_ptr<FriendGroup> enemies;
+};
 
-inline void writeSolution(std::ostream& output, int solution) {
-    output << solution << '\n';
+void fuseGroups(std::vector<std::shared_ptr<FriendGroup>>& people, std::shared_ptr<FriendGroup> intoGroup, std::shared_ptr<FriendGroup> fromGroup) {
+    for (int fid : fromGroup->friends) {
+        intoGroup->friends.insert(fid);
+        people[fid] = intoGroup;
+    }
 }
-
-#define FAIL(output) { writeSolution(output, -1); continue; }
-#define SUCCESS(output, sol) { writeSolution(output, sol); continue; }
-
 
 int main() {
 #ifndef __LOCAL
@@ -25,72 +31,93 @@ int main() {
     std::ostream& output = std::cout;
 #endif
 
-    int nPeople;
+    size_t nPeople;
     input >> nPeople;
 
-    std::vector<std::pair<int, int>> galeti((unsigned long) nPeople);
+    std::vector<std::shared_ptr<FriendGroup>> friendGroupOf(nPeople);
+
     for (int i = 0; i < nPeople; ++i) {
-       galeti[i] = { i, i };
+        friendGroupOf[i] = std::shared_ptr<FriendGroup>(new FriendGroup());
+        friendGroupOf[i]->friends.insert(i);
     }
 
-    int c, x, y;
+    int code, x, y;
     do {
-        input >> c >> x >> y;
-        if (c == 0) {
-            break;
-        }
+        input >> code >> x >> y;
 
-        switch (c) {
+        auto friendsX = friendGroupOf[x];
+        auto friendsY = friendGroupOf[y];
+        auto enemiesX = friendGroupOf[x]->enemies.lock();
+        auto enemiesY = friendGroupOf[y]->enemies.lock();
+
+        bool areFriends = friendsX->friends.count(y) > 0;
+        bool areEnemies = enemiesX != nullptr && enemiesX->friends.count(y) > 0;
+
+        switch (code) {
             case 1:
-            {
-                bool areEnemies = galeti[x].second == galeti[y].second;
                 if (areEnemies) {
-                    output << -1 << "\n";
+                    output << -1 << '\n';
                     break;
                 }
-                int galeata = galeti[y].first;
-                for (int i = 0; i < nPeople; ++i) {
-                    if (galeti[i].first == galeata) {
-                        galeti[i].first = galeti[x].first;
-                        galeti[i].second = galeti[x].second;
+
+                if (areFriends) {
+                    break;
+                }
+
+                // friends of my friend become my friends
+                fuseGroups(friendGroupOf, friendsX, friendsY);
+
+                if (enemiesX != nullptr && enemiesY != nullptr) {
+                    // my friend's enemies become friends of my enemies
+                    if (enemiesX != enemiesY) {
+                        fuseGroups(friendGroupOf, enemiesX, enemiesY);
                     }
+
+                    // my friend's enemies become my enemies
+                    friendsX->enemies = enemiesX;
+                    enemiesX->enemies = friendsX;
+                }
+                else if (enemiesX == nullptr && enemiesY != nullptr) {
+                    // my friend's enemies become my enemies
+                    friendsX->enemies = enemiesY;
+                    enemiesY->enemies = friendsX;
                 }
                 break;
-            }
 
             case 2:
-            {
-                bool areEnemies = galeti[x].first == galeti[y].first;
-                if (areEnemies) {
-                    output << -1 << "\n";
+                if (areFriends) {
+                    output << -1 << '\n';
                     break;
                 }
-                int galeata = galeti[y].second;
-                for (int i = 0; i < nPeople; ++i) {
-                    if (galeti[i].second == galeata) {
-                        galeti[i].first = galeti[x].first;
-                        galeti[i].second = galeti[x].second;
-                    }
+
+                if (areEnemies) {
+                    break;
                 }
+
+                if (enemiesX != nullptr) {
+                    // enemies of my enemy become my friends
+                    fuseGroups(friendGroupOf, friendsY, enemiesX);
+                }
+                if (enemiesY != nullptr) {
+                    // enemies of my enemy become my friends
+                    fuseGroups(friendGroupOf, friendsX, enemiesY);
+                }
+                friendsX->enemies = friendsY;
+                friendsY->enemies = friendsX;
                 break;
-            }
 
-            case 3: {
-
-                bool areEnemies = galeti[x].first == galeti[y].first;
-                output << (areEnemies ? 1 : 0) << "\n";
+            case 3:
+                output << (areFriends ? 1 : 0) << '\n';
                 break;
-            }
 
-            case 4: {
-
-                bool areEnemies = galeti[x].second == galeti[y].second;
-                output << (areEnemies ? 1 : 0) << "\n";
+            case 4:
+                output << (areEnemies ? 1 : 0) << '\n';
                 break;
-            }
 
+            default:
+                break;
         }
-    } while (c != 0);
+    } while(code != 0);
 
     output.flush();
     return 0;
